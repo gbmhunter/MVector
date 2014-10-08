2225f78f2b12858b0e999e266bb722035aef14a3
Geoffrey Hunter <gbmhunter@gmail.com>
2014-10-09 12:29:05 +1300

    Fixed bug where copy constructor not available, compiler does automatic shallow copy and creates memory issues, closes #4. Fixed bug where invalid delete called if MVector was constructed but no elements added before destructed, closes #3.

--- README.rst
+++ README.rst
@@ -11,8 +11,8 @@ A microcontroller-friendly C++ vector object specifically designed for embedded 
 
 - Author: gbmhunter <gbmhunter@gmail.com> (www.mbedded.ninja)
 - First Ever Commit: 2014-09-18
-- Last Modified: 2014-09-18
-- Version: v1.0.2.0
+- Last Modified: 2014-10-09
+- Version: v1.0.3.0
 - Company: mbedded.ninja
 - Project: MToolkit Module
 - Language: C++
@@ -64,6 +64,7 @@ Changelog
 ========= ========== ===================================================================================================
 Version   Date       Comment
 ========= ========== ===================================================================================================
+v1.0.3.0  2014-10-09 Fixed bug where copy constructor not available, compiler does automatic shallow copy and creates memory issues, closes #4. Fixed bug where invalid delete called if MVector was constructed but no elements added before destructed, closes #3.
 v1.0.2.0  2014-09-18 Fixed bug where calling MVector::Erase() with just one element attempts to allocate 0 bytes of memory, closes #2.
 v1.0.1.0  2014-09-18 Removed unneeded includes from 'MVector.cpp'.
 v1.0.0.0  2014-09-18 Initial commit, basic vector with Append() and Erase() implemented, and simple unit tests added, closes #1.
--- include/MVector.hpp
+++ include/MVector.hpp
@@ -2,7 +2,7 @@
 //! @file				MVector.hpp
 //! @author				Geoffrey Hunter <gbmhunter@gmail.com> (www.mbedded.ninja)
 //! @created			2014-09-18
-//! @last-modified		2014-09-18
+//! @last-modified		2014-10-09
 //! @brief
 //! @details
 //!						See README.rst in repo root dir for more info.
@@ -32,15 +32,15 @@ namespace MbeddedNinja
 //========================================== INCLUDES ===========================================//
 //===============================================================================================//
 
-// System headers
+//===== SYSTEM LIBRARIES =====//
 #include <cstdint>		// uint32_t
 #include <cstring>		// memcpy()
-//#include <iostream>
+//#include <iostream>		//! @debug
 
-// User libraries
+//===== USER LIBRARIES =====//
 #include "MAssert/api/MAssertApi.hpp"	// M_ASSERT_FAIL()
 
-// User headers
+//===== USER SOURCE =====//
 // none
 
 //===============================================================================================//
@@ -59,221 +59,221 @@ namespace MbeddedNinja
 		public:
 
 
-			//======================================================================================//
-			//==================================== PUBLIC METHODS ==================================//
-			//======================================================================================//
-					
+		//======================================================================================//
+		//==================================== PUBLIC METHODS ==================================//
+		//======================================================================================//
 
-			//! @brief		Simplified constructor.
-			//! @details
-			MVector() :
-				elementsPtr(nullptr),
-				size(0)
 
+		//! @brief		Simplified constructor.
+		//! @details
+		MVector() :
+			elementsPtr(nullptr),
+			size(0)
+
+		{
+			// Nothing to do here...
+		};
+
+		//! @brief		Copy constructor.
+		//! @details
+		MVector(const MVector &obj) :
+			MVector()
+		{
+			// We need to copy all the elements across
+			for(uint32_t x = 0; x < obj.size; x++)
 			{
-				// Nothing to do here...
-			};
+				this->Append(obj.elementsPtr[x]);
+			}
 
-			//! @brief		Copy constructor.
-			//! @details	Delegates to normal constructor.
-			//MVector(const MVector &obj);
+		}
 
-			//! @brief		Destructor.
-			//! @details	Deletes memory that was allocated for the vectors elements.
-			~MVector()
+		//! @brief		Destructor.
+		//! @details	Deletes memory that was allocated for the vectors elements.
+		~MVector()
+		{
+			if(this->size != 0)
 			{
 				// Delete memory that was allocated in the constructor.
 				delete[] this->elementsPtr;
 			}
+		}
 
-			//! @brief		Call to find how many objects are currently in the vector.
-			//! @returns
-			uint32_t Size()
+		//! @brief		Call to find how many objects are currently in the vector.
+		//! @returns	Number of object that are currently in the vector.
+		uint32_t Size()
+		{
+			// Easy, just return the internal length variable
+			return this->size;
+		}
+
+		//! @brief		Append a object onto the end of the vector.
+		//! @param		objToAppend		A pointer to the object you wish to append to the vector.
+		void Append(const ElementT & objToAppend)
+		{
+			// Special case when Append() is called and there are currently no objects
+			// in the vector, we have to be careful to not call delete[] on old
+			// pointer since memory
+			// is not currently allocated
+			if(this->size == 0)
 			{
-				// Easy, just return the internal length variable
-				return this->size;
-			}
-
-			//! @brief		Append a C-style string onto the end of the current string.
-			//! @param		objToAppend		A pointer to the object you wish to append to the vector.
-			void Append(const ElementT & objToAppend)
-			{
-				// Special case when Append() is called and there are currently no objects
-				// in the vector, we have to be careful to not call delete[] on old
-				// pointer since memory
-				// is not currently allocated
-				if(this->size == 0)
-				{
-					this->elementsPtr = new ElementT[1];
-					memcpy(this->elementsPtr, &objToAppend, sizeof(ElementT));
-					this->size++;
-					return;
-				}
-
-				// Allocate memory for new vector with an additional element
-				ElementT * newElementsPtr = new ElementT[this->size + 1];
-
-				// Copy across existing memory
-				memcpy(newElementsPtr, this->elementsPtr, sizeof(ElementT)*this->size);
-
-				// Copy across second section
-				memcpy(newElementsPtr + this->size, &objToAppend, sizeof(ElementT));
-
-				// Free old memory
-				delete[] this->elementsPtr;
-
-				// Save new pointer
-				this->elementsPtr = newElementsPtr;
-
-				// Update length
+				this->elementsPtr = new ElementT[1];
+				memcpy(this->elementsPtr, &objToAppend, sizeof(ElementT));
 				this->size++;
-
+				return;
 			}
 
-			//! @brief		Erases a specific object from the vector.
-			void Erase(uint32_t index)
+			// Allocate memory for new vector with an additional element
+			ElementT * newElementsPtr = new ElementT[this->size + 1];
+
+			// Copy across existing memory
+			memcpy(newElementsPtr, this->elementsPtr, sizeof(ElementT)*this->size);
+
+			// Copy across second section
+			memcpy(newElementsPtr + this->size, &objToAppend, sizeof(ElementT));
+
+			// Free old memory
+			delete[] this->elementsPtr;
+
+			// Save new pointer
+			this->elementsPtr = newElementsPtr;
+
+			// Update length
+			this->size++;
+
+		}
+
+		//! @brief		Erases a specific object from the vector.
+		void Erase(uint32_t index)
+		{
+			/*std::cout << "MVector::Erase() called." << std::endl;*/
+
+			// Make sure index is not out-of-bounds
+			M_ASSERT(index < this->size);
+
+			// Special case when there is only one element left in the vector and
+			// Erase() is called, vector memory is simply deleted and pointer
+			// set to nullptr.
+			if(this->size == 1)
 			{
-				/*std::cout << "MVector::Erase() called." << std::endl;*/
-
-				// Make sure index is not out-of-bounds
-				M_ASSERT(index < this->size);
-
-				// Special case when there is only one element left in the vector and
-				// Erase() is called, vector memory is simply deleted and pointer
-				// set to nullptr.
-				if(this->size == 1)
-				{
-					delete[] this->elementsPtr;
-					this->elementsPtr = nullptr;
-					return;
-				}
-
-				/*std::cout <<
-					"oldPtr[0] = '" << ((uint32_t *)this->elementsPtr)[0] <<
-					"', oldPtr[1] = '" << ((uint32_t *)this->elementsPtr)[1] <<
-					"', oldPtr[2] = '" << ((uint32_t *)this->elementsPtr)[2] << "'." << std::endl;*/
-
-				ElementT * newElementsPtr = new ElementT[this->size - 1];
-
-				// Make sure memory allocation was successful
-				M_ASSERT(newElementsPtr);
-
-				/*std::cout <<
-					"Going to copy '" <<
-					sizeof(ElementT)*index <<
-					"bytes' from mem address '" <<
-					this->elementsPtr <<
-					"' to mem address '" <<
-					newElementsPtr <<
-					"'." << std::endl;*/
-
-				// Copy everything from the old vector up to (but not including)
-				// the element we wish to remove.
-				// This should work even if index is 0 (nothing will be copied)
-				memcpy(
-					newElementsPtr,					// Write to the start of the new memory space
-					this->elementsPtr,				// Read from the start of the old array
-					sizeof(ElementT)*index);		// Copy everything upto the element we wish to remove
-
-				// Now copy everything from the old vector after the element
-				// we wish to remove
-				/*std::cout <<
-					"Going to copy '" <<
-					sizeof(ElementT)*(this->size - index - 1) <<
-					"bytes' from mem address '" <<
-					this->elementsPtr + index + 1 <<
-					"' to mem address '" <<
-					newElementsPtr + index <<
-					"'." << std::endl;*/
-
-				memcpy(
-					newElementsPtr + index,								// Start writing to the next element
-					this->elementsPtr + index + 1,						// The +1 is to skip over the element we wish to erase
-					sizeof(ElementT)*(this->size - index - 1));
-
-
-				/*std::cout <<
-					"newPtr[0] = '" << ((uint32_t *)newElementsPtr)[0] <<
-					"', newPtr[1] = '" << ((uint32_t *)newElementsPtr)[1] << "'." << std::endl;*/
-
-				// Free old memory
 				delete[] this->elementsPtr;
-
-				// Save new pointer
-				this->elementsPtr = newElementsPtr;
-
-				// Update size
-				this->size--;
-
+				this->elementsPtr = nullptr;
+				this->size = 0;
+				return;
 			}
 
+			/*std::cout <<
+				"oldPtr[0] = '" << ((uint32_t *)this->elementsPtr)[0] <<
+				"', oldPtr[1] = '" << ((uint32_t *)this->elementsPtr)[1] <<
+				"', oldPtr[2] = '" << ((uint32_t *)this->elementsPtr)[2] << "'." << std::endl;*/
 
-			//======================================================================================//
-			//============================ PUBLIC OPERATOR OVERLOAD DECLARATIONS ===================//
-			//======================================================================================//
+			ElementT * newElementsPtr = new ElementT[this->size - 1];
 
-			//! @brief		Assignment operator overload. Allows you to make one vector equal another.
-			//! @details	Copies the contents of the RHS vector into the LHS, so that they both have
-			//!				the same contents (but do not point to the same memory!).
-			//MVector & operator= (const MVector & other);
+			// Make sure memory allocation was successful
+			M_ASSERT(newElementsPtr);
 
-			//! @brief		Subscript operator overload. Allows access to individual elements.
-			//! @returns	Reference to element located at position 'index'. If index is out-of-bounds,
-			//!				assert will be raised.
-			ElementT & operator[] (const uint32_t index)
+			/*std::cout <<
+				"Going to copy '" <<
+				sizeof(ElementT)*index <<
+				"bytes' from mem address '" <<
+				this->elementsPtr <<
+				"' to mem address '" <<
+				newElementsPtr <<
+				"'." << std::endl;*/
+
+			// Copy everything from the old vector up to (but not including)
+			// the element we wish to remove.
+			// This should work even if index is 0 (nothing will be copied)
+			memcpy(
+				newElementsPtr,					// Write to the start of the new memory space
+				this->elementsPtr,				// Read from the start of the old array
+				sizeof(ElementT)*index);		// Copy everything upto the element we wish to remove
+
+			// Now copy everything from the old vector after the element
+			// we wish to remove
+			/*std::cout <<
+				"Going to copy '" <<
+				sizeof(ElementT)*(this->size - index - 1) <<
+				"bytes' from mem address '" <<
+				this->elementsPtr + index + 1 <<
+				"' to mem address '" <<
+				newElementsPtr + index <<
+				"'." << std::endl;*/
+
+			memcpy(
+				newElementsPtr + index,								// Start writing to the next element
+				this->elementsPtr + index + 1,						// The +1 is to skip over the element we wish to erase
+				sizeof(ElementT)*(this->size - index - 1));
+
+
+			/*std::cout <<
+				"newPtr[0] = '" << ((uint32_t *)newElementsPtr)[0] <<
+				"', newPtr[1] = '" << ((uint32_t *)newElementsPtr)[1] << "'." << std::endl;*/
+
+			// Free old memory
+			delete[] this->elementsPtr;
+
+			// Save new pointer
+			this->elementsPtr = newElementsPtr;
+
+			// Update size
+			this->size--;
+
+		}
+
+
+		//======================================================================================//
+		//============================ PUBLIC OPERATOR OVERLOAD DECLARATIONS ===================//
+		//======================================================================================//
+
+		//! @brief		Assignment operator overload. Allows you to make one vector equal another.
+		//! @details	Copies the contents of the RHS vector into the LHS, so that they both have
+		//!				the same contents (but do not point to the same memory!).
+		//MVector & operator= (const MVector & other);
+
+		//! @brief		Subscript operator overload. Allows access to individual elements.
+		//! @returns	Reference to element located at position 'index'. If index is out-of-bounds,
+		//!				assert will be raised.
+		ElementT & operator[] (const uint32_t index)
+		{
+
+			// Check if index is >= size
+			if(index >= this->size)
 			{
+				M_ASSERT_FAIL("%s", "MVector::operator[] index was out-of-bounds.");
 
-				// Check if index is >= size
-				if(index >= this->size)
-				{
-					M_ASSERT_FAIL("%s", "MVector::operator[] index was out-of-bounds.");
-
-					// Index is out of bounds, the best we can do is return
-					// something is in bounds, so let's return
-					// the last char. This could cause unexpected behaviour,
-					// so an assert() needs to be added!
-					return this->elementsPtr[0];
-				}
-
-				return this->elementsPtr[index];
+				// Index is out of bounds, the best we can do is return
+				// something is in bounds, so let's return
+				// the last char. This could cause unexpected behaviour,
+				// so an assert() needs to be added!
+				return this->elementsPtr[0];
 			}
 
+			return this->elementsPtr[index];
+		}
 
-			//======================================================================================//
-			//================================= PUBLIC VARIABLES ===================================//
-			//======================================================================================//
+
+		//======================================================================================//
+		//================================= PUBLIC VARIABLES ===================================//
+		//======================================================================================//
 				
 				
 		private:
 						
-			//======================================================================================//
-			//=================================== PRIVATE METHODS ==================================//
-			//======================================================================================//
-			
-			// none
-			
-			//======================================================================================//
-			//================================== PRIVATE VARIABLES =================================//
-			//======================================================================================//
+		//======================================================================================//
+		//=================================== PRIVATE METHODS ==================================//
+		//======================================================================================//
 
-			ElementT * elementsPtr;
+		// none
 
-			//! @brief		Remembers the number of elements stored in the vector.
-			uint32_t size;
+		//======================================================================================//
+		//================================== PRIVATE VARIABLES =================================//
+		//======================================================================================//
 
-		protected:
+		ElementT * elementsPtr;
 
-			//======================================================================================//
-			//=================================== PROTECTED METHODS ================================//
-			//======================================================================================//
-			
-			// none
-			
-			//======================================================================================//
-			//================================== PROTECTED VARIABLES ===============================//
-			//======================================================================================//
+		//! @brief		Remembers the number of elements stored in the vector.
+		uint32_t size;
 
-			// none
 		
 	}; // class MVector
 
